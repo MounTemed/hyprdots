bd555d110fb7ba8965c845c112615e0052e71b8d
LaiMicK <hyprland@vk.com>
2025-10-27 16:40:56 +0500

    foot: We're reworking it for the Caelestia theme

--- home-manager/home/terminal/foot/default.nix
+++ home-manager/home/terminal/foot/default.nix
@@ -4,29 +4,27 @@
     enable = true;
     settings = {
       main = {
+        shell = "fish";
         font = "FiraCode Nerd Font:size=13";
         term = "foot";
-        pad = "0x0";
+        pad = "5x5";
       };
+
       bell = {
         urgent = "no";
         notify = "no";
       };
-      desktop-notifications = {
-        command = "notify-send {title} {body}";
-      };
+
       scrollback = {
         lines = 1000;
       };
+
       cursor = {
         blink = true;
       };
+
       colors = {
         alpha = 1;
-        background = "101010";
-        foreground = "C9C7CD";
-        selection-foreground = "101010";
-        selection-background = "C9C7CD";
       };
     };
   };
