ecbc01f09b74f702a733f65dff7e082a806b784e
LaiMicK <hyprland@vk.com>
2025-11-01 19:02:12 +0500

    pipeware: setting to default

--- system/modules/pipewire/default.nix
+++ system/modules/pipewire/default.nix
@@ -3,8 +3,9 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    audio.enable = true;
+    pulse.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
   };
 }
