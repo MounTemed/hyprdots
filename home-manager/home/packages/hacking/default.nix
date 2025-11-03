ef17ea001d1e62313da6e45e06fc7404bbb9037d
LaiMicK <hyprland@vk.com>
2025-11-03 21:42:20 +0500

    all: fmt

--- home-manager/home/packages/hacking/default.nix
+++ home-manager/home/packages/hacking/default.nix
@@ -1,4 +1,5 @@
-{ ... }: {
+{ ... }:
+{
   imports = [
     ./nikto.nix
     ./nmap.nix
--- system/modules/desktop/hyprland.nix
+++ system/modules/desktop/hyprland.nix
@@ -3,7 +3,8 @@
   programs.hyprland = {
     enable = true;
     # withUWSM = true;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   xdg.portal = {
