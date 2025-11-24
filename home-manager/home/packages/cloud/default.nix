e56ede3fb36992a7f984d12b332dfe0c0162626d
LaiMicK <hyprland@vk.com>
2025-11-24 23:37:16 +0500

    cloud: init packer and kubebuilder

--- home-manager/home/packages/cloud/default.nix
+++ home-manager/home/packages/cloud/default.nix
@@ -2,5 +2,6 @@
 {
   imports = [
     ./k8s.nix
+    ./packer.nix
   ];
 }
--- home-manager/home/packages/cloud/packer.nix
+++ home-manager/home/packages/cloud/packer.nix
@@ -1,9 +1,6 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    kubernetes
-    kubernetes-helm
-
-    kubebuilder
+    packer
   ];
 }
