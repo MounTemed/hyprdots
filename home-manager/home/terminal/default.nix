c27b962501315045c37ea516fa73f853f93916e3
LaiMicK <hyprland@vk.com>
2025-11-27 22:14:31 +0500

    rio: init and remove foot

--- home-manager/home/terminal/default.nix
+++ home-manager/home/terminal/default.nix
@@ -1,11 +1,7 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    foot
-  ];
-
   imports = [
     ./fish
-    ./foot
+    ./rio
   ];
 }
---
+++ home-manager/home/terminal/rio/default.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    rio
+  ];
+
+  programs.rio = {
+    enable = true;
+
+    settings = {
+      fonts = {
+        size = 13;
+
+        disable-warnings-not-found = {
+          enable = false;
+        };
+      };
+    };
+  };
+
+}
