38df3298c819bac6dec12428426246d8c3cbb564
LaiMicK <hyprland@vk.com>
2025-12-01 22:35:29 +0500

    rio: update

--- home-manager/home/terminal/rio/default.nix
+++ home-manager/home/terminal/rio/default.nix
@@ -25,7 +25,29 @@
         args = [ ];
       };
       colors = {
-        cursor = "#d6d3e6";
+        background = "111318";
+        foreground = "e2e2e9";
+
+        cursor = "acc7ff"; 
+        cursor_foreground = "0f2f60"; 
+
+        black = "353434"; 
+        red = "8a80ff";
+        green = "44def5"; 
+        yellow = "ffdcf2";
+        blue = "8cadd4"; 
+        magenta = "a0a4ef";
+        cyan = "96d0fc";
+        white = "e8d3de";
+
+        light_black = "ac9fa9";
+        light_red = "a59dff";
+        light_green = "89ecff";
+        light_yellow = "fff0f6";
+        light_blue = "aec3da";
+        light_magenta = "b9b9f8";
+        light_cyan = "b9e0ff";
+        light_white = "ffffff";
       };
     };
   };
