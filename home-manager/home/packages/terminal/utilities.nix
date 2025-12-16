9c03f6c5bb7d0999eba5cc09708e51f293c7ac8f
LaiMicK <hyprland@vk.com>
2025-12-16 23:46:26 +0500

    fish: add zoxide, eza, startship

--- home-manager/home/packages/terminal/utilities.nix
+++ home-manager/home/packages/terminal/utilities.nix
@@ -5,4 +5,28 @@
     wget
     fastfetch
   ];
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.eza = {
+    enable = true;
+    enableFishIntegration = true;
+    icons = "auto";
+    git = true;
+  };
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      add_newline = false;
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+      };
+    };
+  };
 }
