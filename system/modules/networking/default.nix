ba8bde5e534862a2d737fc2d4bdbf8981cb58a3b
LaiMicK <hyprland@vk.com>
2025-12-16 23:44:17 +0500

    network: rewrote the logic

--- system/modules/networking/default.nix
+++ system/modules/networking/default.nix
@@ -2,19 +2,31 @@
 {
   networking = {
     hostName = "nixos";
+    
     useDHCP = false;
     dhcpcd.enable = false;
 
     nameservers = [
-      "8.8.8.8"
-      "8.8.4.4"
-      "1.1.1.1"
-      "1.0.0.1"
+      "1.1.1.1"   # Cloudflare Primary
+      "1.0.0.1"   # Cloudflare Secondary
+      "8.8.8.8"   # Google Backup
+      "8.8.4.4"   # Google Backup
     ];
 
     networkmanager = {
       enable = true;
-      dns = "none";
+      dns = "systemd-resolved"; 
     };
+  };
+
+  services.resolved = {
+    enable = true;
+    dnsovertls = "true";
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [
+      "1.1.1.1"
+      "8.8.8.8"
+    ];
   };
 }
