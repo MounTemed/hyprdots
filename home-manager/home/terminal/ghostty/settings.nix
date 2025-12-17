af63753e8e12a142577ce891a0f6c5086fc2e063
LaiMicK <hyprland@vk.com>
2025-12-17 21:26:28 +0500

    ghostty: add color sttings

--- home-manager/home/terminal/ghostty/settings.nix
+++ home-manager/home/terminal/ghostty/settings.nix
@@ -1,2 +1,37 @@
 { ... }: {
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      window-padding-balance = true;
+      window-padding-x = 0;
+      window-padding-y = 0;
+      window-padding-color = "extend";
+      
+      window-decoration = "none"; 
+
+      background = "111318";
+      foreground = "e2e2e9";
+      cursor-color = "acc7ff";
+      cursor-text-color = "0f2f60";
+
+      palette = [
+        "0=#353434"
+        "1=#8a80ff"
+        "2=#44def5"
+        "3=#ffdcf2"
+        "4=#8cadd4"
+        "5=#a0a4ef"
+        "6=#96d0fc"
+        "7=#e8d3de"
+        "8=#ac9fa9"
+        "9=#a59dff"
+        "10=#89ecff"
+        "11=#fff0f6"
+        "12=#aec3da"
+        "13=#b9b9f8"
+        "14=#b9e0ff"
+        "15=#ffffff"
+      ];
+    };
+  };
 }
